9025fe001b9320ad8f6452f7f9a738afc9a2ccb3
Ömer Faruk Merey <omer.merey@siemens.com>
2023-11-02 10:59:14 +0300

    changed the file structure

--- helpers.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def load_json(file_name) -> dict:
-    if not file_name.endswith(".json"):
-        file_name += ".json"
-    file = open(file_name)
-    data = json.load(file)
-    return data
--- words.py
+++ words.py
@@ -1,5 +1,13 @@
 from deep_translator import GoogleTranslator
 import pandas as pd
+import json
+
+def load_json(file_name) -> dict:
+    if not file_name.endswith(".json"):
+        file_name += ".json"
+    file = open(file_name)
+    data = json.load(file)
+    return data
 
 def translate_en_to_tr(word):
     translated_word = GoogleTranslator(source="auto", 
