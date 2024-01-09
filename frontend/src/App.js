e3d6c143815c6c0750be369e6bc18e7a66cb7445
Ömer Davarcı <omerdavarci.user@gmail.com>
2024-01-09 11:12:37 +0300

    draggable button added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import DraggableButton from './components/DraggableButton';
+import useScreenSize from './hooks/useScreenSize';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const screenSize = useScreenSize();
+	document.body.style.overflow = "hidden"
+
+	return (
+		<div className="App" style={{width: screenSize.width, height: screenSize.height}}>
+	  		<DraggableButton screenSize={screenSize}></DraggableButton>
+	  	</div>
+	);
 }
 
 export default App;
---
+++ frontend/src/components/DraggableButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Draggable from 'react-draggable';
+import IconButton from '@mui/material/IconButton';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+
+function DraggableButton({screenSize}) {
+    return(
+        <Draggable bounds='parent' defaultPosition={{x:0, y:0}}>
+            <IconButton color="secondary">
+                <AddCircleIcon fontSize="large"></AddCircleIcon>
+            </IconButton>
+        </Draggable>
+    );
+}
+
+export default DraggableButton; 
