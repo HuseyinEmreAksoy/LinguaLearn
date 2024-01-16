b90addd64cf4a50e7a99664492b84d54bc7f4b8a
Hüseyin Emre Aksoy <h.emreaksoy@hotmail.com>
2024-01-16 15:44:56 +0300

    backend add

---
+++ backend/LinguaBackend/src/test/java/com/lingua/LinguaBackend/LinguaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingua.LinguaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinguaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
