d4b5663000abe65d116edd4ea73810572121813f
Ömer Davarcı <omerdavarci.user@gmail.com>
2024-02-07 14:34:04 +0300

    dummy fetch developed, save bug fixed

--- frontend/src/pages/ReadingPage.js
+++ frontend/src/pages/ReadingPage.js
@@ -7,17 +7,6 @@ import ReplayIcon from '@mui/icons-material/Replay';
 
 const ReadingPage = () => {
 
-    const createQuestion = (question, a, b, c, d, update, bgColor) => {
-        return(<Question
-            class={"mt-5" + " " + bgColor}
-            question={question}
-            update={update} 
-            a={a} 
-            b={b} 
-            c={c} 
-            d={d} />);
-    };
-
     let dummyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tincidunt justo et magna tincidunt, sit amet ornare nisl imperdiet. Integer laoreet enim urna, et placerat justo scelerisque eget. Cras maximus mi a ex porta, vitae convallis orci aliquet. Proin ultricies, massa quis fermentum vulputate, nisl ex interdum purus, quis euismod mauris dui ut felis. Donec fringilla vulputate mi ac auctor. Sed posuere tellus ac rutrum mattis. Aliquam ligula sapien, pulvinar non imperdiet et, laoreet sed nisl. Phasellus dapibus varius leo, a ultricies orci. Etiam sed gravida erat, at gravida dolor. Quisque dapibus felis vitae urna tincidunt tempor. Ut hendrerit, lorem eu tempus bibendum, tortor eros faucibus diam, quis vulputate nibh leo at urna. Ut vitae convallis urna. Sed placerat lacus nulla, venenatis luctus nisl cursus non.";
     let dummyTitle = "Dummy"
 
@@ -25,80 +14,117 @@ const ReadingPage = () => {
     const [text, setText] = useState("");
     const [questions, setQuestions] = useState([]);
     const [answers, setAnswers] = useState([]);
-    const [isSubmitDisable, setIsSubmitDisable] = useState(false);
-    let newAnswers2 = [];
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    let newAnswers = [];
 
-    useEffect(() => {
-        if(answers.length === 0) {
-            setIsSubmitDisable(true);
-            return;
-        }
+    // TO-DO: Write a useEffect to tell whether all the questions answered or not!
+    // useState(() => {
 
-        for(let i = 0; i < answers.length; i++) {
-            if(answers === "") {
-                setIsSubmitDisable(true);
-                return;
-            }
-        }
-        setIsSubmitDisable(false);
-        
-    }, [answers, newAnswers2]);
+    // }, [answers]);
+
+    const createQuestion = (question, a, b, c, d, correctAnswer, update, bgColor) => {
+        return(<Question
+            class={"mt-5" + " " + bgColor}
+            question={question}
+            update={update} 
+            a={a} 
+            b={b} 
+            c={c} 
+            d={d}
+            correctAnswer={correctAnswer} />);
+    };
 
     const update = (value, index) => {
-        console.log("value " + value);
-        console.log("index " + index);
-        // let newAnswers = answers.map((element, i) => {
-        //     if(i === index)
-        //         return value;
-        //     return element;
-        // });
-        let newAnswers = [];
-        for(let i = 0; i < newAnswers2.length + 1; i++) {
+        for(let i = 0; i < newAnswers.length; i++) {
             if(i === index) {
-                newAnswers.push(value);
-            }
-            else {
-                if(newAnswers2[i] !== undefined)
-                    newAnswers.push(newAnswers2[i]);
+                newAnswers[i] = value;
             }
         }
-
         console.log(newAnswers);
-        newAnswers2 = [...newAnswers]
-        setAnswers(newAnswers2);
+        setAnswers(newAnswers);
     } 
 
     const dummyFetch = () => {
         setTitle(dummyTitle);
         setText(dummyText);
-        let newAnswers = [];
         for(let i = 0; i < 2; i++) {
             newAnswers.push("");
         }
-        console.log("first value");
-        console.log(newAnswers);
         setAnswers(newAnswers);
-        let firstQuestion = createQuestion("Who is the founder of LinguaLearn?", "Hüseyin Emre Aksoy", "Ömer Faruk Merey", "Ömer Davarcı", "All of them", (value) => {update(value, 0)}, "");
-        let secondQuestion = createQuestion("Who am I?", "No idea", "just someone", "me", "my princess", (value) => {update(value, 1)}, ""); 
+        let firstQuestion = createQuestion("Who is the founder of LinguaLearn?", "Hüseyin Emre Aksoy", "Ömer Faruk Merey", "Ömer Davarcı", "All of them", "d", (value) => {update(value, 0)}, "");
+        let secondQuestion = createQuestion("Who am I?", "No idea", "just someone", "me", "my princess", "d", (value) => {update(value, 1)}, ""); 
         let dummyQuestions = [firstQuestion, secondQuestion]
         setQuestions(dummyQuestions);
     }
 
+    const turnQuestionRed = (index) => {
+        return changeQuestionColor(index, " bg-red-500");
+    }
+
+    const turnQuestionGreen = (index) => {
+        return changeQuestionColor(index, " bg-green-500");
+    }
+
     const changeQuestionColor = (index, color) => {
         let oldProps = questions[index].props;
-        let newElement = createQuestion(oldProps.question, oldProps.a, oldProps.b, oldProps.c, oldProps.d, oldProps.update, color);
-        const newQuestions = questions.map((question, i) => {
-            if(i === index)
-                return newElement;
-            return question;
-        });
-        setQuestions(newQuestions);
+        return createQuestion(oldProps.question, oldProps.a, oldProps.b, oldProps.c, oldProps.d, oldProps.correctAnswer, oldProps.update, color);
     }
 
     const handleSubmit = () => {
+        console.log(answers);
+        let newQuestions = [];
         for(let i = 0; i < questions.length; i++) {
-            
+            if(questions[i].props.correctAnswer === answers[i]) {
+                newQuestions.push(turnQuestionGreen(i));
+            }
+            else {
+                newQuestions.push(turnQuestionRed(i));
+            }
         }
+        setQuestions(newQuestions);
+        setIsSubmitted(true);
+    }
+
+    //TO-DO: Delete this part of code after establish the connection to backend ****************************************************************
+    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz      ';
+    
+    function generateString(length) {
+        let result = ' ';
+        const charactersLength = characters.length;
+        for ( let i = 0; i < length; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+
+        return result;
+    }
+
+    const delay = ms => new Promise(
+        resolve => setTimeout(resolve, ms)
+      );
+
+     //******************************************************************************************************************************************
+
+    const handleNew = async () => {
+        setQuestions([]);
+        setAnswers([]);
+        setText("");
+        setTitle("");
+        setIsSubmitted(false);
+        newAnswers = [];
+
+        await delay(5000);
+
+        let newQuestions = [];
+        let numberOfQuestions = 5;
+        for(let i = 0; i < numberOfQuestions; i++) {
+            newAnswers.push("");
+            newQuestions.push(createQuestion(generateString(50), generateString(10), generateString(4), generateString(14), generateString(19), "c", 
+            (value) => {update(value, i);}, ""));
+        }
+        setAnswers(newAnswers);
+        setQuestions(newQuestions);
+        setText(generateString(500));
+        setTitle(generateString(10));
     }
 
     return(
@@ -114,20 +140,15 @@ const ReadingPage = () => {
                     <div class="flex justify-center mt-20">
                         <h1>{title}</h1>
                     </div>
-                    <div class="grid cols-1 justify-start mt-5 ml-40 mr-40">
-                        <p>{text}</p>
-                        <div class="mt-10">
+                    <div class="grid cols-12 justify-start mt-5 ml-40 mr-40">
+                        <p class="col-span-12">{text}</p>
+                        <div class="mt-10 col-span-12">
                             {questions}
                         </div>
-                        <div class="justify-center grid grid-cols-2 gap-4">
-                            <Button onClick={handleSubmit} disabled={isSubmitDisable}>YÜKLE</Button>
-                            <Button onClick={dummyFetch} startIcon={<ReplayIcon></ReplayIcon>}>YENİ</Button>
+                        <div class="col-span-6 col-start-4 justify-center grid grid-cols-2 gap-4 mb-10 mt-5">
+                            <Button onClick={handleSubmit}>YÜKLE</Button>
+                            <Button onClick={handleNew} startIcon={<ReplayIcon></ReplayIcon>}>YENİ</Button>
                         </div>
-                    </div>
-                    <div>
-                        <Button onClick={() => {changeQuestionColor(0, "bg-red-500");}}>Turn Red</Button>
-                        <Button onClick={() => {changeQuestionColor(0, "bg-green-500")}}>Turn Green</Button>
-                        <Button onClick={() => {console.log(answers);}}>answer</Button>
                     </div>
                 </Wrapper>
             }
