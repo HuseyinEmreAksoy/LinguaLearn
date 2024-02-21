f59e22eb3447d2a9597d73a4ffc8dd742d63a26b
Ömer Davarcı <omerdavarci.user@gmail.com>
2024-02-21 11:05:51 +0300

    words loads automatically

--- frontend/src/pages/VocabularyPage.js
+++ frontend/src/pages/VocabularyPage.js
@@ -1,7 +1,7 @@
 import { Button } from "@mui/material";
 import FlashCard from "../components/FlashCard";
 import FullPage from "../components/Helper/FullPage"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DraggableButton from "../components/DraggableButton";
 import ReplayIcon from '@mui/icons-material/Replay';
 import SendIcon from '@mui/icons-material/Send';
@@ -10,18 +10,33 @@ import axios from "axios";
 
 const VocabularyPage = (props) => {
     const numberOfWord = 6;
-    // TO-DO: 6 words will be given per page. A word will contain 2 string ["front", "back"]
 
-    const dummyWords = [["Mouse", "Fare"], ["Yellow", "Sarı"], ["Backpack", "Sırt Çantası"], ["Apple", "Elma"], ["Water", "Su"], ["Who", "Kim"]];
+    // const dummyWords = [["Mouse", "Fare"], ["Yellow", "Sarı"], ["Backpack", "Sırt Çantası"], ["Apple", "Elma"], ["Water", "Su"], ["Who", "Kim"]];
     let initialAnswers = [];
     for(let i = 0; i < numberOfWord; i++) {
         initialAnswers.push("");
     }
     
-    const [words, setWords] = useState(dummyWords);
+    const [words, setWords] = useState([]);
     const [answers, setAnswers] = useState(initialAnswers);
     const [isSubmitted, setIsSubmitted] = useState(false);
     const [correntAnswer, setCorrectAnswer] = useState(-1);
+
+    useEffect(() => {
+        handleNew();
+    }, []);
+
+    const getNRandomNumber = (n, upperLimit) => {
+        let toReturn = [];
+        for(let i = 0; i < n; i++) {
+            let rand = -1;
+            while(rand === -1 || toReturn.includes(rand)) {
+                rand = Math.floor(Math.random() * (upperLimit + 1));
+            }
+            toReturn.push(rand);
+        }
+        return toReturn;
+    };
 
     const handleSubmit = () => {
         setIsSubmitted(true);
@@ -32,16 +47,21 @@ const VocabularyPage = (props) => {
             }
         }
         setCorrectAnswer(numberOfCorrectAnswer);
-    }    
+    };
 
-    const fetchNewWords = async () => {
+    const handleNew = async () => {
         setWords([]);
-         
         const response = await axios.get("http://localhost:8080/api/v1/word/findByLevel?wordLevel=B2&wordLanguage=English");
-        console.log(response);
-        
+        let randomIndexes = getNRandomNumber(numberOfWord, response.data.length);
+        let newWords = [];
+        for(let i = 0; i < numberOfWord; i++) {
+            let word = response.data[randomIndexes[i]];
+            newWords.push([word.wordWord, word.wordTranslation]);
+        }
+
+        setWords(newWords);
         setIsSubmitted(false);
-    }
+    };
 
     const updateAnswer = (value, index) => {
         let newAnswers = [];
@@ -52,7 +72,7 @@ const VocabularyPage = (props) => {
                 newAnswers.push(value);
         }
         setAnswers(newAnswers);
-    }
+    };
 
     //Initilize cards
     let cardElements = [];
@@ -75,7 +95,7 @@ const VocabularyPage = (props) => {
                 {cardElements}
                 <div class="col-start-2 justify-center grid grid-cols-2 gap-4">
                     <Button onClick={handleSubmit} disabled={words.length === 0 || isSubmitted} startIcon={<SendIcon></SendIcon>}>GÖNDER</Button>
-                    <Button onClick={fetchNewWords} startIcon={<ReplayIcon></ReplayIcon>}>YENİ</Button>
+                    <Button onClick={handleNew} startIcon={<ReplayIcon></ReplayIcon>}>YENİ</Button>
                 </div>
                 {
                     isSubmitted ? 
