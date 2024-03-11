85143a96109cf202823d552c8f952a952518b973
Ömer Davarcı <omerdavarci.user@gmail.com>
2024-03-11 17:56:34 +0300

    subject removed from grammarPage

--- frontend/src/pages/WritingPage.js
+++ frontend/src/pages/WritingPage.js
@@ -10,43 +10,11 @@ import axios from "axios";
 
 const WritingPage = () => {
 
-    const [subject, setSubject] = useState("");
     const [text, setText] = useState("");
     const [isSubmitted, setIsSubmitted] = useState(false);
-    const [correctedText, setCorrectedText] = useState("");
-
-    useEffect(() => {
-        handleNew();
-    }, []);
 
     const handleChange = (event) => {
         setText(event.target.value);
-    }
-
-    //TO-DO: Delete this part of code after establish the connection to backend ****************************************************************
-    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz      ';
-    
-    function generateString(length) {
-        let result = ' ';
-        const charactersLength = characters.length;
-        for ( let i = 0; i < length; i++ ) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-
-        return result;
-    };
-
-    const delay = ms => new Promise(
-        resolve => setTimeout(resolve, ms)
-      );
-
-     //******************************************************************************************************************************************
-
-    const handleNew = async () => {
-        setIsSubmitted(false);
-        setCorrectedText("");
-        setText("");
-        setSubject(generateString(Math.floor(Math.random() * 10) + 20));
     }
 
     const handleSubmit = async () => {
@@ -62,39 +30,30 @@ const WritingPage = () => {
             <DraggableButton></DraggableButton>
             <div style={{marginLeft: "17%"}} class="w-8/12">
                 <div class="grid cols-12">
-                    <div class="col-span-12 flex justify-start mt-5 mb-7 ml-5">
-                        <h1>{subject}</h1>
+                    <div class="col-span-12">
+                        <TextField
+                            id="outlined-multiline-flexible"
+                            label="Your Text"
+                            fullWidth
+                            multiline
+                            rows={25}
+                            onChange={handleChange}
+                            value={text}
+                            sx={{
+                                "& .MuiInputBase-root": {
+                                    height: "30%"
+                                }
+                            }}
+                        />
                     </div>
-                    {
-                        true ?
-                            <Wrapper>
-                                <div class="col-span-12">
-                                    <TextField
-                                        id="outlined-multiline-flexible"
-                                        label="Your Text"
-                                        fullWidth
-                                        multiline
-                                        rows={23}
-                                        onChange={handleChange}
-                                        value={text}
-                                        sx={{
-                                            "& .MuiInputBase-root": {
-                                                height: "30%"
-                                            }
-                                        }}
-                                    />
-                                </div>
-                                <div class="col-span-6 col-start-4 justify-center grid grid-cols-2 gap-4 mb-3 mt-5">
-                                    <Button onClick={handleSubmit} disabled={text==="" || isSubmitted} startIcon={<SendIcon></SendIcon>}>GÖNDER</Button>
-                                    <Button onClick={handleNew} startIcon={<ReplayIcon></ReplayIcon>}>YENİ</Button>
-                                </div>
-                            </Wrapper>
-                        :
-                            <div class="col-span-12">
-                                <p>{correctedText}</p>
-                            </div>
-                    }
-                    
+                    <div class="col-span-6 col-start-4 justify-center grid grid-cols-1 gap-4 mb-3 mt-5">
+                        {
+                            isSubmitted ?
+                                <Button onClick={handleSubmit} disabled={true}>YÜKLENİYOR</Button>
+                            :
+                                <Button onClick={handleSubmit} disabled={text === ""} startIcon={<SendIcon></SendIcon>}>GÖNDER</Button>
+                        }
+                    </div>
                 </div>
             </div> 
         </FullPage>
